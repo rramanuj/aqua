f848b0223bce2fd4dd734d90c3575b6df36894e7
rramanuj <rasesh94@gmail.com>
2018-06-06 00:51:34 +0100

    Initial Commit

---
+++ AquaTests/AquaTests.swift
@@ -0,0 +1,36 @@
+//
+//  AquaTests.swift
+//  AquaTests
+//
+//  Created by Rasesh Ramanuj on 06/06/2018.
+//  Copyright © 2018 Rasesh Ramanuj. All rights reserved.
+//
+
+import XCTest
+@testable import Aqua
+
+class AquaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
